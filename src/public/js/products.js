680e37eba08959754d3eb02ba00611ddb2fab77d
Katieln <139283838+Katieln@users.noreply.github.com>
2024-06-08 22:54:32 -0500

    ajustes

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -16,14 +16,14 @@ document.addEventListener('DOMContentLoaded', function() {
     function showprods(listProds) {
       listProds.forEach(prod => {
         boxprods.innerHTML += `
-          <div class="card col-md-4" style="width: 15rem; margin: 10px;">
+          <div class="product-card col-md-4" style="width: 15rem; margin: 10px;">
             <img src="${prod.image}" class="card-img-top" alt="${prod.title}" />
-            <div class="card-body">
+            <h5 class="card-title text-center">${prod.title.toUpperCase()}</h5>
+            <div class="product-card-body">
             
-              <h5 class="card-title text-center">${prod.title.toUpperCase()}</h5>
-              <p class="card-text">${prod.description}</p>
-              <p class="card-text">Price: $${prod.price}</p>
-              <p class="card-text">Category: ${prod.category}</p>
+                          <p class="product-card-text">${prod.description}</p>
+              <p class="product-card-text">Price: $${prod.price}</p>
+              <p class="product-card-text">Category: ${prod.category}</p>
               <button data-product-id="${prod._id}" class="btn btn-primary comprar">Comprar</button>
             </div>
           </div>
